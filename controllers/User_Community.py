45867456ac4cb5bb572700fcf3ed72508a530f4b
Carlos <csandovalc@unal.edu.co>
2023-05-15 20:25:29 -0500

    feat(user): the user can leave a community

--- controllers/User_Community.py
+++ controllers/User_Community.py
@@ -40,3 +40,42 @@ class UserEnterCommunity(Resource):
             return user_belongs_to_community.json(), 201
         except:
             return {"message": "An error occurred creating the user."}, 500
+
+
+class UserLeaveCommunity(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument(
+        "community_id", type=int, required=True, help="This field cannot be blank."
+    )
+    parser.add_argument(
+        "user_id", type=int, required=True, help="This field cannot be blank."
+    )
+
+    @jwt_required()
+    def delete(self):
+        data = UserEnterCommunity.parser.parse_args()
+        jwt_user = get_jwt_identity()
+        user_id = jwt_user["id"]
+
+        if user_id != data["user_id"]:
+            return {"message": "User not found"}, 404
+
+        if not CommunityModel.find_by_id(data["community_id"]):
+            return {"message": "Community not found"}, 404
+
+        if not UserModel.find_by_id(data["user_id"]):
+            return {"message": "User not found"}, 404
+
+        user_belongs_to_community = (
+            UserBelongsToCommunityModel.find_by_user_id_and_community_id(
+                data["user_id"], data["community_id"]
+            )
+        )
+        if not user_belongs_to_community:
+            return {"message": "User not in community"}, 400
+
+        try:
+            user_belongs_to_community.delete_from_db()
+            return {"message": "User left community"}, 200
+        except:
+            return {"message": "An error occurred leaving the community."}, 500
