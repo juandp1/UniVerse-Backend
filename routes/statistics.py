e230fab844d512b3235da8f4f50b237f775ad44f
GianK19 <glanziano@unal.edu.co>
2023-06-12 14:19:04 -0500

    Add routes for num topics and users in community

--- routes/statistics.py
+++ routes/statistics.py
@@ -1,5 +1,9 @@
 from controllers.Statistics import QuestionsPerCommunity
+from controllers.Statistics import UsersPerCommunity
+from controllers.Statistics import TopicsPerCommunity
 
 
 def add_resources(api):
     api.add_resource(QuestionsPerCommunity, "/api/statistics/questions_per_comm/")
+    api.add_resource(TopicsPerCommunity, "/api/statistics/topics_per_comm/")
+    api.add_resource(UsersPerCommunity, "/api/statistics/users_per_comm/")
