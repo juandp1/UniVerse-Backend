401a23195af5873f9709a842db5132e2f80d4f3b
Carlos <csandovalc@unal.edu.co>
2023-06-18 22:28:07 -0500

    fix: wrong name of module

--- config/routes_conf.py
+++ config/routes_conf.py
@@ -1,7 +1,7 @@
 from routes.auth import add_resources as add_auth_resources
 from routes.community import add_resources as add_community_resources
 from routes.encyclopedia import add_resources as add_encyclopedia_resources
-from routes.label import add_resources as add_label_resources
+from routes.labels import add_resources as add_label_resources
 from routes.meeting import add_resources as add_meeting_resources
 from routes.questions import add_resources as add_questions_resources
 from routes.responses import add_resources as add_responses_resources
