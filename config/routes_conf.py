56091ea80930c9bb40b30034c8facc036ec5ae28
Carlos <csandovalc@unal.edu.co>
2023-06-18 22:25:24 -0500

    fix: enable routes of label

--- config/routes_conf.py
+++ config/routes_conf.py
@@ -1,6 +1,7 @@
 from routes.auth import add_resources as add_auth_resources
 from routes.community import add_resources as add_community_resources
 from routes.encyclopedia import add_resources as add_encyclopedia_resources
+from routes.label import add_resources as add_label_resources
 from routes.meeting import add_resources as add_meeting_resources
 from routes.questions import add_resources as add_questions_resources
 from routes.responses import add_resources as add_responses_resources
@@ -13,6 +14,7 @@ def start_routes(api):
     add_auth_resources(api)
     add_community_resources(api)
     add_encyclopedia_resources(api)
+    add_label_resources(api)
     add_meeting_resources(api)
     add_questions_resources(api)
     add_responses_resources(api)
